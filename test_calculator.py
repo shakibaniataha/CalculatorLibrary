e20f6cb8d34e4d9e767ace0415a3b3e096e91f23
taha <t.shakiba@irantalent.com>
2020-02-24 15:15:44 +0330

    some wrong commit to make circleci fail!

--- test_calculator.py
+++ test_calculator.py
@@ -11,3 +11,6 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 5 == calculator.subtract(12, 7)
+
+    def test_multiplication(self):
+        assert 100 == calculator.multiply(10, 10)
